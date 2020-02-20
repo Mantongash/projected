5ad5ea2590ff4295bd51126fe02610d35151312a
Mantongash <you@example.com>
2020-02-20 08:14:16 +0300

    Modified logout and login views

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import home, register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('', home, name='home'),
+  path('', auth_views.LoginView.as_view(template_name='users/signin.html'), name='signup'),
+  path('', auth_views.LogoutView.as_view(template_name='home.html'), name='signout'),
   path('register/', register, name='register')
 ]
