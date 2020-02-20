fe96e5f98181a55f0e7bb4c43fcf279e3aba05c8
Mantongash <you@example.com>
2020-02-20 08:32:01 +0300

    Corrected numerous bugs

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('', home, name='home'),
-  path('', auth_views.LoginView.as_view(template_name='users/signin.html'), name='signup'),
+  path('', auth_views.LoginView.as_view(template_name='users/signin.html'), name='signin'),
   path('', auth_views.LogoutView.as_view(template_name='home.html'), name='signout'),
   path('register/', register, name='register')
 ]
