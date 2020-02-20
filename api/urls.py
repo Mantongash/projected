e35d2b16f9fe73e14356931d91a686053fd2555b
Mantongash <you@example.com>
2020-02-20 07:28:05 +0300

    Fixed url bug

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import ListProjects, DetailProjects, ListUsers
 
 urlpatterns = [
   path('<int:pk>/', DetailProjects.as_view()),
-  path('/projects', ListProjects.as_view()),
-  path('/users', ListUsers.as_view()),
+  path('projects/', ListProjects.as_view()),
+  path('users/', ListUsers.as_view()),
   ]
