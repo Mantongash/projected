c5b13cfe57f901d0cc784bf18e93db23f9a384f0
Mantongash <you@example.com>
2020-02-20 15:17:26 +0300

    Fixed images on home page

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
   path('', home, name='home'),
   path('signin/', auth_views.LoginView.as_view(template_name='users/signin.html'), name='signin'),
-  path('signout/', auth_views.LogoutView.as_view(template_name='home.html'), name='signout'),
+  path('', auth_views.LogoutView.as_view(template_name='home.html'), name='signout'),
   path('register/', register, name='register'),
   path('profile/', profile, name='profile'),
   path('edit_profile/', edit_profile, name='edit_profile'),
